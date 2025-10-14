2de1c0ebcb3e2fb2f457a09af8df608df21b639b
matteobnvl <matteobnvl@users.noreply.github.com>
2025-10-14 16:12:50 +0000

    deploy: d9d941ea9c07710b4a15d7574a99712026152163

---
+++ src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["rust_project",["",[],["base.rs","main.rs","map.rs","robot.rs","utils.rs"]]]]');
+//{"start":19,"fragment_lengths":[77]}
---
+++ trait.impl/core/hash/trait.Hash.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rust_project",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"rust_project/robot/struct.RobotPosition.html\" title=\"struct rust_project::robot::RobotPosition\">RobotPosition</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[304]}
